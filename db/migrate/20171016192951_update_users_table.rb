7ee0606fe291a4b674ad1b111a91667ef3823164
Diane Lenertz <lenertzd@grinnell.edu>
2017-10-16 12:40:20 -0700

    fix users activerecord table

---
+++ db/migrate/20171016192951_update_users_table.rb
@@ -0,0 +1,8 @@
+class UpdateUsersTable < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016185021) do
+ActiveRecord::Schema.define(version: 20171016193459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "sessions", force: :cascade do |t|
-    t.string   "username"
-    t.string   "name"
-    t.string   "email"
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
   end
 
   create_table "votes", force: :cascade do |t|
